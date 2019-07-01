7dfab6dac99f0470fd717ddc6d8643614a1624fc
pdmurray <peynmurray@gmail.com>
2019-07-01 22:29:38 +0300

    Added brightness control script, bound to the Pause/Break button.

---
+++ .config/brightness_toggle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BRIGHTNESS=`xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+NIGHTVALUE=0.6
+DAYVALUE=1.0
+
+if [ `echo "$BRIGHTNESS == 1.0" | bc` -eq 1 ]
+then
+        SETVALUE=$NIGHTVALUE
+else
+        SETVALUE=$DAYVALUE
+fi
+
+xrandr --output DP-0 --brightness $SETVALUE
