716a850616b251d277ef786ac8e9df10d8b5cd4e
pdmurray <peynmurray@gmail.com>
2020-08-21 18:40:11 -0700

    Greatly simplified symbolicize.sh, added ignore config file.

--- symbolicize.sh
+++ symbolicize.sh
@@ -6,50 +6,33 @@
 # If it doesn't exist, make a symbolic link, creating required directories
 # as necessary.
 
-if [[ $# > 0 ]]; then
-	echo "Dry run:";
-fi
+ignores=$(sed '/^#/d' ./symbolicize.conf)
+
+DRY=0
+while getopts "yd" option; do
+    case $option in
+        d)
+            echo "Dry run."
+            DRY=1
+            ;;
+    esac
+done
+
 
 for item in $(ls -A ./); do
 
-    if [[ -e $HOME/$item ]]; then
-		echo "${HOME}/${fname}->${pwd}/${fname}";
+    # If the item is not in the ignore list, make a link
+    if [[ ${ignores} != *"${item}"* ]]; then
 
+        linkpath=${HOME}/${item}
+        targetpath=$(pwd)/${item}
 
-		if [[ $# == 0 ]]; then
-			rm ${HOME}/${fname} 2>/dev/null;
-			ln -sf $(pwd)/${fname} ${HOME}/${fname};
-		fi
-
-
-
-
-for item in $(find ./ -type f ! -path './.git/*' ! -path './symbolicize.sh' ! -path './.gitignore'); do
-	# Strip the './' from the files found in the current directory
-	fname=${item:2};
-
-	# If the file exists in $HOME, then try to replace it with a symbolic link to the
-	# corresponding file in ${pwd}
-	if [[ -e $HOME/${fname} ]]; then
-		echo "${HOME}/${fname}->${pwd}/${fname}";
-
-		# Don't replace if any arguments are passed to the script; this is a dry run
-		if [[ $# == 0 ]]; then
-			rm ${HOME}/${fname} 2>/dev/null;
-			ln -sf $(pwd)/${fname} ${HOME}/${fname};
-		fi
-
-	# If the file doesn't exist in $HOME, ask the user about making a symbolic link anyway,
-	# creating directories as necessary
-	else
-		read -p "${HOME}/${fname} doesn't exist. Create symbolic link anyway? [Y/n]: " makelink
-		if [[ $(echo ${makelink} | awk '{print tolower($0)}') == "y" ]] || [[ $makelink == "" ]]; then
-			echo "${HOME}/${fname}->$(pwd)/${fname}";
-			if [[ $# == 0 ]]; then
-				echo "Making directory $(dirname ${HOME}/${fname})"
-				mkdir -pv $(dirname ${HOME}/${fname});
-				ln -sf $(pwd)/${fname} ${HOME}/${fname};
-			fi
-		fi
-	fi
+        if [[ "${DRY}" == 0 ]]; then
+            rm -rfv ${linkpath}
+            mkdir -pv $(dirname ${linkpath});
+            ln -sfv ${targetpath} ${linkpath};
+        else
+		    echo "${linkpath}->${targetpath}";
+        fi
+    fi
 done
