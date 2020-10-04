740d3d338674b7b75166e9073d4d857abf8a041a
pdmurray <peynmurray@gmail.com>
2020-10-04 13:37:26 -0700

    Switched to bspwm. Moved volume/brightness scripts together.

--- .config/vpn_toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-status=$(sudo protonvpn s)
-if [[ $(echo ${status} | head -1) == 'Connected' ]]; then
-    sudo protonvpn d >/dev/null 2>&1
-else
-    sudo protonvpn c -f >/dev/null 2>&1
-fi
