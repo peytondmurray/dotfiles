3a0ecf1b5a8cb1ea78fd3102f954bd368556dfd9
pdmurray <peynmurray@gmail.com>
2021-07-28 11:04:50 -0600

    Added efm-langserver+eslint_d+tsserver for js development

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -62,6 +62,49 @@ nvim_lsp['sumneko_lua'].setup {
 
 nvim_lsp['tsserver'].setup{}
 
+local eslint = {
+    lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+    lintStdin = true,
+    lintFormats = {"%f:%l:%c: %m"},
+    lintIgnoreExitCode = true,
+    formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+    formatStdin = true
+}
+
+local function eslint_config_exists()
+    local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+
+    if not vim.tbl_isempty(eslintrc) then
+        return true
+    end
+
+    if vim.fn.filereadable("package.json") then
+        if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+            return true
+        end
+    end
+
+    return false
+end
+
+nvim_lsp['efm'].setup{
+    init_options = {documentFormatting = true},
+    filetypes = {"javascript", "typescript"},
+    root_dir = function()
+        if not eslint_config_exists() then
+            return nil
+        end
+        return vim.fn.getcwd()
+    end,
+    settings = {
+        rootMarkers = {".eslintrc.js", ".git/"},
+        languages = {
+            javascript = {eslint},
+            typescript = {eslint}
+        }
+    }
+}
+
 require('compe').setup {
     enabled = true;
     autocomplete = true;
