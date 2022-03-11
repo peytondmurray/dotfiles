8a32a13c6ca1765a00a86c4046ac40c195f266a6
pdmurray <peynmurray@gmail.com>
2022-03-11 11:48:52 -0800

    Export `has_words_before` from luautils - it's needed for cmp

--- .config/nvim/lua/luautils.lua
+++ .config/nvim/lua/luautils.lua
@@ -12,7 +12,7 @@ local ReplaceTC = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-function has_words_before()
+function M.has_words_before()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
