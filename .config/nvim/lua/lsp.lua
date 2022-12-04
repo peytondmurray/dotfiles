d8ca0cff030297a3feb38a4ad18d9147ae96d73b
pdmurray <peynmurray@gmail.com>
2022-12-04 11:11:14 -0800

    Add sumneko lua lsp config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -153,6 +153,29 @@ nvim_lsp['cmake'].setup{
     capabilities = capabilities,
 }
 
+nvim_lsp['sumneko_lua'].setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 -- Load snippets given by friendly-snippets
 require('luasnip/loaders/from_vscode').lazy_load()
 
