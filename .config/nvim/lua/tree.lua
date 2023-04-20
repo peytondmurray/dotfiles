df209b2e91312b7032b478c8b6f9febfdfa461ce
pdmurray <peynmurray@gmail.com>
2023-04-19 23:05:43 -0700

    Add keybinding for grimshot copy area; adjust nvimtree options

--- .config/nvim/lua/tree.lua
+++ .config/nvim/lua/tree.lua
@@ -2,8 +2,6 @@ local tree_cb  = require('nvim-tree.config').nvim_tree_callback
 require('nvim-tree').setup {
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {},
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = false,
