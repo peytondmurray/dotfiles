d7858c6f911030937fd192788bf8aeab42d51648
pdmurray <peynmurray@gmail.com>
2025-04-14 10:23:41 -0700

    Include hidden files in find_files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -369,6 +369,12 @@ require('telescope').setup{
     colorscheme = {
         enable_preview = true,
     },
+    pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
     winblend = 20,
     defaults = {
         file_ignore_patterns = {
