460c2593cf78f00855a69483424d83f8ad926bdb
pdmurray <peynmurray@gmail.com>
2025-11-03 12:59:40 -0800

    Add noice.nvim; add remote branch names when pulling

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,11 +14,37 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
     {
-        "vigoux/notifier.nvim",
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        -- "rcarriga/nvim-notify",
+        },
         config = function()
-            require('notifier').setup({})
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
         end
     },
+
     -- {
     --     "luukvbaal/statuscol.nvim",
     --     config = function()
@@ -167,6 +193,8 @@ require("lazy").setup({
             })
         end
     },
+
+    -- Themes
     -- 'savq/melange',
     --  'Mofiqul/dracula.nvim'
     --  'EdenEast/nightfox.nvim'
@@ -202,6 +230,7 @@ require("lazy").setup({
                     hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
                 }
             })
+            require('mini.notify')
         end
     },
     -- Git Integration
