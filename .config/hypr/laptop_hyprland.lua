4957dce9e7a4c1c98ea98cf1247dfa5265dbfacc
pdmurray <peynmurray@gmail.com>
2026-07-14 22:27:37 -0700

    Unset __GLX_VENDOR_LIBRARY_NAME on laptop

--- .config/hypr/laptop_hyprland.lua
+++ .config/hypr/laptop_hyprland.lua
@@ -76,10 +76,6 @@ end)
 hl.env("XCURSOR_SIZE", "12")
 hl.env("HYPRCURSOR_SIZE", "12")
 
--- https://wiki.hypr.land/Nvidia/#environment-variables
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-
 -- https://wiki.hypr.land/Nvidia/#flickering-in-electron--cef-apps
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
